d9a269f3edffd673848923ceabc5b41ee919dbca
utsavmajhi <utsavmajhi@gmail.com>
2020-01-30 18:50:39 +0530

    Done editing bugs

--- blockchain/app/testapi/api.js
+++ blockchain/app/testapi/api.js
@@ -3,7 +3,7 @@ const routes = require('./routes')
 const PORT = "3000"
 const api = express()
 const logger= (req,res,next)=>{
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`)
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}:${res.body}`)
     next()
 }
 api.use(express.json())
